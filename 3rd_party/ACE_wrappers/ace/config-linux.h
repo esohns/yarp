3cd51777df23596a85104af889ade93a05555d9c
Erik Sohns <eriksohns@123mail.org>
2015-05-29 01:08:27 +0100

    minor changes

--- 3rd_party/ACE_wrappers/ace/config-linux.h
+++ 3rd_party/ACE_wrappers/ace/config-linux.h
@@ -5,6 +5,15 @@
 // it has several drawbacks (see relevant documentation)
 #define ACE_HAS_REACTOR_NOTIFICATION_QUEUE
 
-// *TODO*: libpthread now supports thread names
+// *NOTE*: libpthread now supports thread names
 //         (see /usr/include/pthread.h::453: pthread_setname_np())
 //#define ACE_HAS_PTHREAD_ATTR_SETNAME
+
+// *NOTE*: set FD_SETSIZE so select-based reactors can dispatch more than the
+//         default (1024, see below) handles
+//#include <bits/typesizes.h>
+#include <sys/types.h>
+#undef __FD_SETSIZE
+#define __FD_SETSIZE 65536
+#include <sys/select.h>
+//#include <linux/posix_types.h>
